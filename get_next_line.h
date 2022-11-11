fac1aa4b1861c139ada4f92509279fe3b0da840b
Abdülkadir Yılmaz <akadiryilmaz@Abdulkadir-MacBook-Pro.local>
2022-11-11 19:30:05 +0300

    get_next_line commit

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayilmaz <42istanbul.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 15:22:47 by ayilmaz           #+#    #+#             */
+/*   Updated: 2022/03/08 15:22:49 by ayilmaz          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 9999
+# endif
+
+char	*ft_strjoin(char *s1, const char *s2);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
+
+#endif
